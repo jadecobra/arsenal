229342cb1db9f86e2849543ea880b9e5446c3a21
jadecobra <jakeitegsy@yahoo.com>
2022-01-27 13:04:43 -0600

    add status for awscliv2 install

--- installAWSCLIv2/installAWSCLIv2OnWindows.ps1
+++ installAWSCLIv2/installAWSCLIv2OnWindows.ps1
@@ -1,4 +1,4 @@
 $aws_cli = "AWSCLIV2.msi"
 curl "https://awscli.amazonaws.com/$aws_cli" -o $aws_cli
-msiexec /i $aws_cli
+Start-Process msiexec -ArgumentList "/i $aws_cli /passive" -PassThru -Wait
 Remove-Item $aws_cli
