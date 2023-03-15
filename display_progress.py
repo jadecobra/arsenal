f274c8fa5f49d2dd1f5e316ad838b8d10fd9c009
jadecobra <jakeitegsy@yahoo.com>
2023-03-15 15:16:26 -0500

    update gitignore

---
+++ display_progress.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import threading
+
+
+class DisplayProgressPercentage(object):
+
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            try:
+                percentage = (self._seen_so_far / self._size) * 100
+            except ZeroDivisionError:
+                percentage = 0
+            sys.stdout.write(
+                f"\r{self._filename} {self._seen_so_far} / {self._size} {percentage:2.2%}"
+            )
+            sys.stdout.flush()
