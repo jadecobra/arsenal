d1a34de6c090a4669d3d358d58bf57cab2b7a110
jadecobra <jakeitegsy@yahoo.com>
2023-04-19 21:33:18 -0500

    update code

--- install_debian/InstallDebianOnWsl.ps1
+++ install_debian/InstallDebianOnWsl.ps1
@@ -1,9 +1,34 @@
-# Open Powershell as Administrator
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-curl https://aka.ms/wsl-debian-gnulinux -o debian.zip
-Expand-Archive ./debian.zip ./debian
-cd debian
-Add-AppxPackage ./DistroLauncher-Appx_1.12.1.0_x64.appx
-$userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
-[System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";C:\Users\Administrator\debian", "User")
+# Installs Debian/Ubuntu on Windows via WSL2
+
+function delimiter() { Write-Host "---------------------------------" }
+function installPackage([string]$distribution) {
+    delimiter
+    Write-Host "getting $distribution archive ..."
+    delimiter
+
+    if ($distribution -eq "ubuntu") { $path="wslubuntu"}
+    if ($distribution -eq "debian") { $path="wsl-debian-gnulinux"}
+
+    curl https://aka.ms/$path -L -o "./$distribution.zip"
+    Expand-Archive "./$distribution.zip" -DestinationPath ./$distribution -Force
+    cd $distribution
+    $package=ls *._x64.appx
+    Import-Module Appx -UseWindowsPowerShell
+    Add-AppxPackage $package
+    wsl --install --distribution $distribution
+    cd ..
+    Remove-Item $distribution -Force -Recurse
+
+    $userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
+    [System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";C:\Users\Administrator\$distribution", "User")
+}
+
+cd ~/Downloads
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+Enable-WindowsOptionalFeature -Online FeatureName Microsoft-Windows-Subsystem-Linux
+Get-WindowsOptionalFeature -Online FeatureName Microsoft-Windows-Subsystem-Linux
+
+installPackage debian
+# installPackage ubuntu
+
 debian
